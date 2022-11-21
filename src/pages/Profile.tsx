f5f86affd6948c3fd7004457ad57cc1d77ab33e9
pavel_trifonov <cjbassfly@gmail.com>
2022-11-21 17:23:48 +0600

    profile page implement

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,3 +1,55 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { auth } from "../firebase";
+import s from "./profile.module.scss";
+
 export const Profile = () => {
-  return <div>Profile</div>;
+  const [formData, setFormData] = useState<{
+    name: string | null | undefined;
+    email: string | null | undefined;
+  }>({
+    name: auth.currentUser?.displayName,
+    email: auth.currentUser?.email,
+  });
+  const navigate = useNavigate();
+
+  const onLogOutHandler = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  return (
+    <>
+      <section className={s.profile}>
+        <h1 className={s.profile__header}>my profile</h1>
+        <div className={s.profile__wrapper}>
+          <form className={s.profile__form}>
+            <input
+              className={s.profile__inputName}
+              type="text"
+              id="name"
+              value={formData.name!}
+              disabled
+            />
+            <input
+              className={s.profile__inputEmail}
+              type="email"
+              id="email"
+              value={formData.email!}
+              disabled
+            />
+            <div className={s.profile__operations}>
+              <p className={s.profile__changeName}>
+                do you want to change your name?{" "}
+                <span className={s.profile__edit}>edit</span>{" "}
+              </p>
+              <p className={s.profile__signOut} onClick={onLogOutHandler}>
+                sign out
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 };
